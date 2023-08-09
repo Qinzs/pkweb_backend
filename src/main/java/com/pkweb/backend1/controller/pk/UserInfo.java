2e2232bc9704092006d7780f780d132a0f013b3e
zsqin <1597868182@qq.com>
2023-08-09 15:01:47 +1000

    first commit

---
+++ src/main/java/com/pkweb/backend1/controller/pk/UserInfo.java
@@ -0,0 +1,17 @@
+package com.pkweb.backend1.controller.pk;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/pk")
+@RestController
+public class UserInfo {
+    @RequestMapping("/user")
+    public Map<String, String> getUser() {
+        Map<String, String> user = new HashMap<>();
+        user.put("name", "Pankaj Kumar");
+        user.put("email", "111");
+        return user;
+    }
+}
