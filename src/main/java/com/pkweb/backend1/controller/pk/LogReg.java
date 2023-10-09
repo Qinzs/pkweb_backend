e6915e0453ae7693e223f9748b0eb032ef1ba070
Angelo <87752289+Angelo236@users.noreply.github.com>
2023-10-09 14:34:07 +1100

    Log and Reg

---
+++ src/main/java/com/pkweb/backend1/controller/pk/LogReg.java
@@ -0,0 +1,39 @@
+package com.pkweb.backend1.controller.pk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/users")
+public class LogReg {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+        String password = request.get("password");
+        String email = request.get("email");
+
+        User user = userService.registerUser(username, password,email);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> request) {
+        String username = request.get("username");
+        String password = request.get("password");
+
+        User user = userService.loginUser(username, password);
+
+        return ResponseEntity.ok(user);
+    }
+}
+
---
+++ src/main/java/com/pkweb/backend1/controller/pk/UserRepository.java
@@ -0,0 +1,10 @@
+package com.pkweb.backend1.controller.pk;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
+
