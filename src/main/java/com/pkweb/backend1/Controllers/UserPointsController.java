5ddcf3a8a1e8ff895bdadf0d08c2239fa44a6360
Angelo <87752289+Angelo236@users.noreply.github.com>
2023-10-09 21:50:23 +1100

    new structure

---
+++ src/main/java/com/pkweb/backend1/Controllers/UserPointsController.java
@@ -0,0 +1,44 @@
+package com.pkweb.backend1.Controllers;
+
+import com.pkweb.backend1.Services.UserPointsDTO;
+import com.pkweb.backend1.Services.UserPointsService;
+import com.pkweb.backend1.controller.pk.User;
+import com.pkweb.backend1.pojo.UserPoints;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/points")
+public class UserPointsController {
+    @Autowired
+    private UserPointsService userPointsService;
+
+    @GetMapping("/user/{userId}")
+    public int getUserPoints(@PathVariable Long userId) {
+        // 通过用户ID查询用户的积分
+        User user = new User();
+        user.setId(userId); // 设置用户ID
+        UserPoints userPoints = userPointsService.getUserPointsByUser(user);
+
+        if (userPoints != null) {
+            return userPoints.getPoints();
+        } else {
+            // 处理积分不存在的情况，可以返回默认值或其他适当的响应
+            return 0;
+        }
+    }
+
+    @GetMapping("/percentile/{userId}")
+    public double getPlayerPercentile(@PathVariable Long userId) {
+        double percentile = userPointsService.calculatePercentile(userId);
+        return percentile;
+    }
+
+    @GetMapping("/leaderboard/top15")
+    public List<UserPointsDTO> getTop15Players() {
+        List<UserPointsDTO> topPlayers = userPointsService.getTop15Players();
+        return topPlayers;
+    }
+}
+
