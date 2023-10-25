479cd7c9410a5b488e77b765ec098fc16b0138ea
Angelo <87752289+Angelo236@users.noreply.github.com>
2023-10-26 02:37:49 +1100

    new userpoints

--- src/main/java/com/pkweb/backend1/Services/UserPointsService.java
+++ src/main/java/com/pkweb/backend1/Services/UserPointsService.java
@@ -3,16 +3,21 @@ package com.pkweb.backend1.Services;
 import com.pkweb.backend1.dto.UserPointsDTO;
 import com.pkweb.backend1.Entity.User;
 import com.pkweb.backend1.Entity.UserPoints;
+import com.pkweb.backend1.Repositories.UserMapper;
 import com.pkweb.backend1.Repositories.UserPointsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class UserPointsService {
     @Autowired
     private UserPointsRepository userPointsRepository;
+
+    @Autowired
+    UserMapper userMapper;
 
     public UserPoints getUserPointsByUser(User user) {
         return userPointsRepository.findByUser(user);
@@ -22,47 +27,33 @@ public class UserPointsService {
         // 查询数据库中所有玩家的积分，并按积分降序排列
         List<UserPoints> allUserPoints = userPointsRepository.findAllByOrderByPointsDesc();
 
-        if (allUserPoints.isEmpty()) {
+        if (allUserPoints == null || allUserPoints.isEmpty()) {
             // 如果没有玩家积分数据，则返回0百分比
             return 0.0;
         }
 
         // 查找当前玩家的积分
-        UserPoints currentUserPoints = userPointsRepository.findByUser_UserId(userId);
-
-        if (currentUserPoints == null) {
-            // 如果当前玩家的积分数据不存在，则返回0百分比
-            return 0.0;
-        }
-
-        // 获取当前玩家的积分
-        int userPoints = currentUserPoints.getPoints();
-
-        // 计算当前玩家在排名中的位置
-        int userRank = 1;
-        for (UserPoints userPoint : allUserPoints) {
-            if (userPoint.getPoints() > userPoints) {
-                userRank++;
-            } else {
-                break;
+        for (int i = 0; i < allUserPoints.size(); i++) {
+            UserPoints userPoints = allUserPoints.get(i);
+            // 获取当前用户的排名
+            if (userPoints.getId().equals(userId)){
+                return   (double)(allUserPoints.size()-1-i)/(allUserPoints.size()-1) * 100;
             }
         }
-
-        // 计算百分比位置
-        double percentile = (double) userRank / allUserPoints.size() * 100.0;
-
-        return percentile;
+        return 0.0;
     }
 
     public List<UserPointsDTO> getTop15Players() {
         // 查询数据库中前十五名玩家的积分数据，并按积分降序排列
         List<UserPoints> topPlayers = userPointsRepository.findTop15ByOrderByPointsDesc();
 
+
         // 转换为DTO对象
         List<UserPointsDTO> topPlayersDTO = topPlayers.stream()
-                .map(userPoints -> new UserPointsDTO(userPoints.getUser().getUsername(), userPoints.getPoints()))
+                .map(userPoints -> new UserPointsDTO(userPoints.getUser().getUsername(), userPoints.getPoints(),userPoints.getUser().getProfile()))
                 .collect(Collectors.toList());
         return topPlayersDTO;
     }
 
 }
+
--- src/main/java/com/pkweb/backend1/dto/UserPointsDTO.java
+++ src/main/java/com/pkweb/backend1/dto/UserPointsDTO.java
@@ -3,8 +3,17 @@ package com.pkweb.backend1.dto;
 public class UserPointsDTO {
     private String username;
     private int points;
+    // 头像
+    private byte[] profile;
+
 
     public UserPointsDTO(String username, int points) {
+    }
+
+    public UserPointsDTO(String username, int points, byte[] profile) {
+        this.username = username;
+        this.points = points;
+        this.profile = profile;
     }
 
     // Getter方法
@@ -23,5 +32,13 @@ public class UserPointsDTO {
 
     public void setPoints(int points) {
         this.points = points;
+    }
+
+    public byte[] getProfile() {
+        return profile;
+    }
+
+    public void setProfile(byte[] profile) {
+        this.profile = profile;
     }
 }
